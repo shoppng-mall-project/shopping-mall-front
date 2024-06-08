433fab90c068580e5fb2d4efff86930195f31b5b
Huro0906 <huro0906@gmail.com>
2024-06-08 14:36:15 +0900

    Feat : menu_util 바 추가
    헤더 상단 카트, 오더, 로그인, 검색 기능 버튼

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,45 @@
 import '../styles/Header.css';
+import cartIcon from '../assets/icon_cart.png';
+import searchIcon from '../assets/icon_search.png';
+import orderIcon from '../assets/icon_order.png';
+
 
 const Header = function (){
     return(
         <div className="inner_head">
             <div className='inner_wrap'>
                 <div className='title_logo'>
-                    <a href="http://localhost:3001/">
+                    <a href="/">
                         <img 
                         src="https://st.kakaocdn.net/commerce_ui/front-talkstore/real/20240605/131847/assets/images/pc/pc_logo_231212.png" 
                         art ="메인로고" />
                     </a>
+                </div>
+                <div className='menu_util'>
+                    <span className='icon'>
+                        <a href='/'>
+                            <img 
+                            src={searchIcon}
+                            art='search'></img>
+                        </a>
+                    </span>
+                    <span className='icon'>
+                        <a href='/'>
+                            <img 
+                            src={cartIcon}
+                            art='cart'></img>
+                        </a>
+                    </span>
+                    <span className='icon'>
+                        <a href='/'>
+                            <img 
+                            src={orderIcon}
+                            art='order'></img>
+                        </a>
+                    </span>
+                </div>
+                <div className='menu_my'>
+                    <a href='/login'>로그인</a>
                 </div>
             </div>
         </div>
