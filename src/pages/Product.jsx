6c8b09d2d7ec4b361cb726796b09c56c31be7187
Huro0906 <huro0906@gmail.com>
2024-06-09 13:14:27 +0900

    Feat : product페이지에 적용
    +image, fontsize 인식을 위한 editorjs 에서 tool 불러오기

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,10 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import Editor from '../components/productDetail/ProducDetail';
+import { getDummyData } from '../components/productDetail/dummyData';
 import './Product.css'
 
 const Product = function (){
+    const [productDetailData, setProductDetailData] = useState();
+    
+    useEffect( () => {
+        setProductDetailData(getDummyData());
+    }, [])
+
     return(
-        <h1>
-            Product Page
-        </h1>
+        <div>
+            <h1>Product Page</h1>
+            <Editor
+                data = {productDetailData}
+            />
+        </div>
     );
 }
 
